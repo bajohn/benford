436d9b832597c3784f185baf9ccae67d28c8680b
Brendan John <bjohn454@gmail.com>
2021-01-11 23:21:36 -0500

    finally update pattern working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 import { selectAll } from 'd3';
+import { exit } from 'process';
+import { concat } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,15 +10,17 @@ import { selectAll } from 'd3';
 })
 export class AppComponent implements OnInit {
 
-
+  private rectData: iRect[] = [];
 
   constructor() {
 
   }
 
+
   ngOnInit() {
     this.initAxes();
     this.initCircles();
+    setInterval(this.mClick.bind(this), 1000);
   }
 
   initCircles() {
@@ -24,7 +28,6 @@ export class AppComponent implements OnInit {
     for (let i = 1; i < 100; i = i * 2) {
       data.push({ x: i, y: 20 });
     }
-    //console.log(this.data);
     const resp = this.selectSvg().
       selectAll('circle').
       data(data).
@@ -34,35 +37,68 @@ export class AppComponent implements OnInit {
       attr('cx', (d: iCirc) => d.x).
       attr('cy', (d: iCirc) => d.y).
       attr('r', 5);
-    console.log(resp);
   }
 
   initAxes() {
-    this.selectSvg().
-      selectAll('rect').
-      data(this.axisData(3)).
-      enter().
-      append('rect').
-      attr('fill', 'black').
-      attr('height', 100).
-      attr('width', 2).
-      attr('x', d => d).
-      attr('y', 100)
+    const movingRect: iRect = {
+      id: -1,
+      x: 0,
+      y: 0,
+      h: 200,
+      w: 5,
+      color: 'red'
+    }
+    this.rectData = this.axisData(3).concat(movingRect);
+    this.rectAppender()
+
   }
 
-  randLoc() {
+  randLoc(): number {
     return 1000 * Math.random();
   }
 
-  axisData(orders: number) {
+  rectAppender() {
+    //console.log('begin', this.rectData);
+
+    const cs = this.selectSvg().
+      selectAll('rect').
+      data(this.rectData, (d: iRect) => d.id).
+      join(
+        enter => enter.
+          append('rect').
+          attr('fill', d => d.color).
+          attr('height', d => d.h).
+          attr('width', d => d.w).
+          attr('x', d => d.x).
+          attr('y', d => d.y),
+        update => {
+          console.log(update);
+          return update.
+            attr('fill', d => d.color).
+            attr('height', d => d.h).
+            attr('width', d => d.w).
+            attr('x', d => d.x).
+            attr('y', d => d.y)
+        },
+        exit => exit.remove(),
+      );
+  }
+
+  axisData(orders: number): iRect[] {
     const factor = 100;
-    const ret = [];
+    const ret: iRect[] = [];
     for (let i = 1; i <= orders; i++) {
       for (let j = 1; j <= 10; j += 1) {
-        ret.push((factor * Math.log(j)) + factor * Math.log(10) * i);
+        ret.push({
+          id: j + (10 * i),
+          x: (factor * Math.log(j) + factor * Math.log(10) * i),
+          y: 100,
+          h: 100,
+          w: 3,
+          color: 'black'
+        });
       }
     }
-    console.log(ret);
     return ret;
   }
 
@@ -70,7 +106,12 @@ export class AppComponent implements OnInit {
     return d3.select('.svg-container');
   }
 
+  mClick() {
 
+    this.rectData[this.rectData.length - 1].x = this.randLoc();
+    console.log(this.randLoc())
+    this.rectAppender();
+  }
 }
 
 interface iCirc {
@@ -79,6 +120,10 @@ interface iCirc {
 }
 
 interface iRect {
+  id: number
   x: number
   y: number
+  h: number
+  w: number
+  color?: string
 }
